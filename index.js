c762ccff96eb1bdd3312fafccc99176085a21ca7
HuynhLe12 <84954268+HuynhLe12@users.noreply.github.com>
2021-06-28 13:42:48 +0700

    main

--- index.js
+++ index.js
@@ -7,29 +7,6 @@ const router = express.Router();
 const port = process.env.PORT  || 8888;
 
 //
-app.use(express.static(path.join(__dirname ,'public')));
-app.engine('hbs', exphbs({
-    extname:'.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname ,'resources','views'))
-
-//
-router.get('/',(req,res)=>{
-    res.render('home');
-});
-router.get('/addcustomer',(req,res)=>{
-    res.render('addcustomer');
-});
-router.get('/addproduct',(req,res)=>{
-    res.render('addproduct');
-});
-router.get('/viewproduct',(req,res)=>{
-    res.render('viewproduct');
-});
-router.get('/viewcustomer',(req,res)=>{
-    res.render('viewcustomer');
-});
-app.use('/', router);
+ 
 app.listen(port);
 console.log(`my app is running at port ${port}`);
