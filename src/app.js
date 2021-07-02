a7e991ad6732f78aa32fc2bccc80ddf369eaefc4
HuynhLe12 <84954268+HuynhLe12@users.noreply.github.com>
2021-07-02 12:03:40 +0700

    main

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const path = require('path')
 const db = require('../config/db')
+app.use(express.urlencoded())
+app.use(express.json())
 var resultsjson
 // Using pg DataBase
 db.query("SELECT * FROM product ORDER BY id ASC", (error, results) =>{
@@ -15,11 +17,11 @@ db.query("SELECT * FROM product ORDER BY id ASC", (error, results) =>{
 })
 
 // Add Values into Database
-const Id = "GCS190156"
-const product_name = "Sang"
-const category = "1"
+/*const Id = "123"
+const product_name = "QuangHuy"
+const category = "people"
 const price = 1000
-const descriptions = "Postgres Database"
+const descriptions = "Gia Lai"
 db.query("INSERT INTO product (id, product_name, category, price, descriptions) VALUES ($1, $2, $3, $4, $5)",
 [Id, product_name, category, price, descriptions],
 (error, results) => {
@@ -29,7 +31,7 @@ db.query("INSERT INTO product (id, product_name, category, price, descriptions) 
     }
     console.log(results)
     }
-)
+)*/
 // Delete Values From DB
 // db.query("DELETE FROM product WHERE id = $1", [Id], (error, results) =>{
 //     if(error){
@@ -67,6 +69,27 @@ app.get('/listorder', function(req, res){
 })
 app.get('/addproduct', function(req,res){
     res.render('addproduct')
+
+})
+app.post('/addproduct', function(req,res){
+    res.render('addproduct')
+    const postId = req.body.proid
+    const postproName = req.body.proname
+    const postcategory = req.body.procategory
+    const postprice = req.body.proprice
+    const postdescriptions = req.body.prodes
+    // INSERT
+    db.query("INSERT INTO product (id, product_name, category, price, descriptions) VALUES ($1, $2, $3, $4, $5)",
+    [postId, postproName, postcategory, postprice, postdescriptions],
+    (error, results) => {
+        if(error){
+            console.log(error)
+            return;
+        }
+        console.log(results)
+        }
+    )
+    res.redirect('/')
 })
 app.get('/listproduct', function(req, res){
     res.render('listproduct')
